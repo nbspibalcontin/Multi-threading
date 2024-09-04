28691167e0b2d44495d9dc493dc3666c7c81c68d
ebenezer.v.alcontin <ebenezer.v.alcontin@MIS-DEV>
2024-09-04 16:45:06 +0800

    Updated

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,12 +1,9 @@
-﻿using ConsoleApp2.LINQ.Implementation;
-using ConsoleApp2.LINQ.Interface;
-using ConsoleApp2.MultiThreading.Implementation;
+﻿using ConsoleApp2.MultiThreading.Implementation;
 using ConsoleApp2.MultiThreading.Interface;
 
 public class Program
 {
     private static readonly INumberProcessor numberProcessor = new NumberProcessor();
-    private static readonly IProduct productService = new ProductService();
     
     public static void Main(string[] args)
     {
@@ -19,20 +16,6 @@ public class Program
             int localNumber = number; // Capture the current number for the thread
             Thread thread = new Thread(() => numberProcessor.ProcessNumber(localNumber));
             thread.Start();
-        }
-
-        Console.WriteLine();
-
-        // LINQ
-
-        double minimumPrice = 500.00;
-
-        var expensiveProducts = productService.GetProductNamesAbovePrice(minimumPrice);
-
-        Console.WriteLine("Products with price greater than or equal to " + minimumPrice + ":");
-        foreach (var productName in expensiveProducts)
-        {
-            Console.WriteLine(productName);
         }
     }
 }
